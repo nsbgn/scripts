53b1452b4bf177d7a4c8c85d9546f1d6524b2189
nsv <nsv@valk.ws>
2019-01-21 18:17:59 +0100

    Further simplify xcwd.sh

--- xcwd.sh
+++ xcwd.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-# Returns the current working directory of the focused urxvtc window.
+# Returns the current working directory of the focused urxvtc window (and
+# perhaps other software too).
 
 # Written because https://github.com/schischi/xcwd does not work for urxvtc,
 # due to it being a daemon. Inspired by https://github.com/wknapik/lastcwd and
@@ -8,13 +9,11 @@
 
 # We first find the window ID of the active window. Then we find the newest
 # process that has the same window ID in its initial environment, and has its
-# /proc/$PID/cwd set.
+# /proc/$PID/cwd set. Otherwise, we just return the home directory.
 
-DIR="$(for PID in $(ps e --sort=+pid -o pid,args \
-    | sed -n "s|^\s*\([0-9]\+\) .*WINDOWID=$(xdotool getactivewindow).*|\\1|p")
-do
-    readlink /proc/$PID/cwd
-done | tail -n 1)"
-
-[ -d "$DIR" ] && echo "$DIR" || echo "$HOME"
-
+(echo "$HOME"
+for PID in $(\
+    ps e --sort=+pid -o pid,args | \
+    sed -n "s|^\s*\([0-9]\+\) .*WINDOWID=`xdotool getactivewindow`.*|\\1|p")
+do readlink /proc/$PID/cwd
+done ) | tail -n 1
