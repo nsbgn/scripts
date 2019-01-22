056ac7853c072301dfe75a5b93cbbc4ff547a492
nsv <nsv@valk.ws>
2019-01-22 13:14:34 +0100

    xcwd.sh process may not be a child of the root.
    This avoids some problems with spawned daemons.

--- xcwd.sh
+++ xcwd.sh
@@ -11,11 +11,13 @@
 # process that has the same window ID in its initial environment, and has its
 # /proc/$PID/cwd set. Otherwise, we just return the home directory.
 
-# This only works if the terminal didn't happen to have spawned another process
-# in the background that is newer than the one you are working with.
+# This only works if the terminal didn't spawn another process in the
+# background that also has a cwd and is newer than the process you are actually
+# working with. To filter out at least some of these cases, we set -N --ppid 1
+# (to say that the process must not be a direct child of the root process)
 
 (echo "$HOME"
- ps e --sort=+pid -o pid,args \
+ ps e -N --ppid 1 --sort=+pid -o pid,args \
   | sed -n "s|^\s*\([0-9]\+\) .*WINDOWID=`xdotool getactivewindow`.*|\\1|p" \
   | xargs -d'\n' -I{} readlink /proc/{}/cwd 
 ) | tail -n 1
