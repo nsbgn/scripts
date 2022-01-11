500264f9a6c71c1e5fcbf15c857ed6dc226e5f41
nsv <nsv@valk.ws>
2022-01-11 16:30:48 +0100

    Update xcwd.

--- xcwd.sh
+++ xcwd.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
+# Returns the current working directory of the focused terminal window. Works
+# for urxvtc and takes into account foreground process.
+# Inspired by:
+# - https://github.com/wknapik/lastcwd
+# - https://gist.github.com/viking/5851049
 
-# Hacky wrapper around xcwd to work with urxvtc. First tries to draw from the
-# window title, then uses xcwd. Only works when your window titles are set up
-# like mine (see dotfiles).
-
-# Written because https://github.com/schischi/xcwd does not work for urxvtc,
-# due to it being a daemon. Inspired by https://github.com/wknapik/lastcwd and
-# https://gist.github.com/viking/5851049, but faster. See git history for the
-# previous, also imperfect way using window IDs.
-
-CWD="$(xdotool getactivewindow getwindowname \
-    | tr -s ' ' | cut -d' ' -f2 | sed "s|^~|$HOME|g")"
-if [ -f "${CWD}" ]; then
-    dirname "${CWD}"
-elif [ -d "${CWD}" ]; then
-    echo "${CWD}"
-else
-    xcwd
-fi
-
+readlink /proc/$(ps e -o tpgid=,tty=,args= \
+    | awk -F' ' '$2~/pts/ && /WINDOWID='$(xdotool getactivewindow)'/ {print $1}' \
+    | head -n1)/cwd || echo $HOME
